bae542f3fb43c6b9b603f083ba9fe9121ad5fe8c
Yuya Asanuma <yuya.asanuma@devenues.com>
2022-05-31 14:59:06 +0900

    simple-from.component作成

---
+++ my-app/src/app/simple-from/simple-from.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-simple-from',
+  templateUrl: './simple-from.component.html',
+  styleUrls: ['./simple-from.component.sass']
+})
+export class SimpleFromComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
