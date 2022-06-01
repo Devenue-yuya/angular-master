5a0a9400d95f671f3e2d5497a2266d19d70ddcf8
Yuya Asanuma <yuya.asanuma@devenues.com>
2022-06-01 12:23:14 +0900

    8-1完了

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SimpleFromComponent } from './simple-from/simple-from.component';
+import { ControlsComponent } from './controls/controls.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SimpleFromComponent
+    SimpleFromComponent,
+    ControlsComponent
   ],
   imports: [
     BrowserModule,
