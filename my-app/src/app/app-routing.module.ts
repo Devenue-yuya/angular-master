c200bfbdb3613cd51821391ae639a39095935332
Yuya Asanuma <yuya.asanuma@devenues.com>
2022-06-02 12:46:05 +0900

    9-1完了

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/Common';
+
 import { RouterModule, Routes } from '@angular/router';
+import { SimpleFromComponent } from './simple-from/simple-from.component';
+import { ControlsComponent } from './controls/controls.component';
+
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    CommonModule,
+    RouterModule.forRoot([
+      {path:'simple-from', component:SimpleFromComponent},
+      {path:'controls', component:ControlsComponent},
+    ]
+  )
+],
+  exports: [
+    RouterModule
+  ],
+  declarations: []
 })
 export class AppRoutingModule { }
